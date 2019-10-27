cf1e269752075e66cd62dd9b0a665a328e861825
teplicate <artem.sakhipov@vit.im>
2019-10-27 15:02:34 +0500

    initial

---
+++ app/src/main/java/ru/teplicate/martialarttracker/fragments/StatisticFragment.kt
@@ -0,0 +1,27 @@
+package ru.teplicate.martialarttracker.fragments
+
+
+import android.graphics.drawable.ShapeDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import ru.teplicate.martialarttracker.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class StatisticFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_statistic, container, false)
+    }
+
+
+}
